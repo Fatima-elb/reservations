8052a76a302cf81d1ca1e1706721320680d3f997
Fatima EL BOUDAATI <2802faelboudaati@student.epfc.eu>
2026-03-19 16:58:03 +0100

    Mapping artist table

---
+++ app/Models/Artist.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Artist extends Model
+{
+    protected $fillable = [
+        'firstname',
+        'lastname',
+    ];
+
+    protected $table = 'artists';
+
+    public $timestamps = false;
+}
