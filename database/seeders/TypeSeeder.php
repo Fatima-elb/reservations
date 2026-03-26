93bab52e919cfaa07f186dd673d51b892d4abaab
Fatima EL BOUDAATI <2802faelboudaati@student.epfc.eu>
2026-03-26 16:51:48 +0100

    ArtistSeeder + Model, migration et seeder de Type

---
+++ database/seeders/TypeSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class TypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $data = [
+            ["type" => "comédien"],
+            ["type" => "scénographe"],
+            ["type" => "auteur"],
+        ];
+        //Vider la table
+        DB::table('types')->truncate();
+
+
+        //Insérer les données dans la table
+        DB::table('types')->insert($data);
+    }
+}
